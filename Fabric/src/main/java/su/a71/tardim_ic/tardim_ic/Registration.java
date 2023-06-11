ed5f002edc88e09e2c2a40cf74537ccfbf5bff33
Andrew-71 <andrey.nikitin.vladimirovich@gmail.com>
2023-06-11 13:57:33 +0300

    How many seconds in eternity?

--- Fabric/src/main/java/su/a71/tardim_ic/tardim_ic/Registration.java
+++ Fabric/src/main/java/su/a71/tardim_ic/tardim_ic/Registration.java
@@ -1,34 +1,18 @@
 package su.a71.tardim_ic.tardim_ic;
 
-import com.swdteam.tardim.common.block.BlockTardimDoors;
-import com.swdteam.tardim.common.block.BlockTardimFloor;
-import com.swdteam.tardim.common.block.BlockTardimRoof;
-import com.swdteam.tardim.common.init.TardimRegistry;
-import com.swdteam.tardim.tileentity.TileEntityTardim;
-
-
-import com.mojang.datafixers.types.Type;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.Util;
-import net.minecraft.core.BlockPos;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.util.datafix.fixes.References;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.core.Registry;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Material;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
 import su.a71.tardim_ic.tardim_ic.jammer.LocationJammerMaterial;
 import su.a71.tardim_ic.tardim_ic.redstone_input.RedstoneInputBlock;
@@ -36,12 +20,11 @@ import su.a71.tardim_ic.tardim_ic.redstone_input.RedstoneInputTileEntity;
 import su.a71.tardim_ic.tardim_ic.Constants;
 import su.a71.tardim_ic.tardim_ic.registration.CommandInit;
 import su.a71.tardim_ic.tardim_ic.registration.ComputerCraftCompat;
+import su.a71.tardim_ic.tardim_ic.registration.CreateCompat;
 import su.a71.tardim_ic.tardim_ic.registration.Exteriors;
-import su.a71.tardim_ic.tardim_ic.soviet_chronobox.SovietChronoboxTileEntity;
 
 public class Registration {
     // Blocks
-
     public static final Block REDSTONE_TARDIM_INPUT = new RedstoneInputBlock();
 
     // Tile Entities
@@ -69,6 +52,9 @@ public class Registration {
 
         if (FabricLoader.getInstance().isModLoaded("computercraft")) {
             ComputerCraftCompat.register();  // Register ComputerCraft-related features
+        }
+        if (FabricLoader.getInstance().isModLoaded("create")) {
+            CreateCompat.register();  // Register Create-related features
         }
         Exteriors.register();  // Register custom TARDIM exteriors
 
--- Fabric/src/main/java/su/a71/tardim_ic/tardim_ic/mixin/JammerMixin.java
+++ Fabric/src/main/java/su/a71/tardim_ic/tardim_ic/mixin/JammerMixin.java
@@ -2,7 +2,10 @@ package su.a71.tardim_ic.tardim_ic.mixin;
 
 import com.swdteam.tardim.common.command.tardim.CommandLocate;
 import com.swdteam.tardim.common.command.tardim.CommandTardimBase;
+import com.swdteam.tardim.tardim.TardimData;
+import com.swdteam.tardim.tardim.TardimManager;
 import dan200.computercraft.api.lua.LuaException;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -14,16 +17,19 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import su.a71.tardim_ic.tardim_ic.registration.CommandInit;
 
 import static com.swdteam.tardim.common.command.tardim.CommandTardimBase.sendResponse;
+import static su.a71.tardim_ic.tardim_ic.Constants.LOG;
 import static su.a71.tardim_ic.tardim_ic.Registration.LOCATION_JAMMER;
 
 @Mixin(value = CommandLocate.class, remap = false)
 public class JammerMixin {
-    @Inject(method="execute([Ljava/lang/String;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lcom/swdteam/tardim/common/command/tardim/CommandTardimBase/CommandSource;)V", at=@At(value = "INVOKE", target = "Lcom/swdteam/tardim/tardim/TardimData;setTravelLocation(Lcom/swdteam/tardim/tardim/TardimData/Location)V"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void init(CallbackInfo ci, Player player, Player otherPlayer, CommandTardimBase.CommandSource source) {
-        for (ItemStack armour : player.getArmorSlots()) {
-            if (armour.is(LOCATION_JAMMER)) {
-                sendResponse(player, "Player's location is jammed", CommandTardimBase.ResponseType.FAIL, source);
-            };
-        }
+    @Inject(method="execute()V", at=@At(value = "INVOKE", target = "Lcom/swdteam/tardim/tardim/TardimData;setTravelLocation(Lcom/swdteam/tardim/tardim/TardimData$Location;)V"))
+    public void execute(CallbackInfo ci) {
+        LOG.info("test");
+//        for (ItemStack armour : player.getArmorSlots()) {
+//            if (armour.is(LOCATION_JAMMER)) {
+//                sendResponse(player, "Player's location is jammed", CommandTardimBase.ResponseType.FAIL, source);
+//                ci.cancel();
+//            };
+//        }
     }
 }
